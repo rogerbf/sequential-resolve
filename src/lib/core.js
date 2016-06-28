7ab78671bf689b4bc40aeb0621b146991e7fe09d
rogerbf <roger@bergfalk.nu>
2016-06-28 18:32:20 +0200

    error handling, getting element value instead of array, update description

--- src/lib/core.js
+++ src/lib/core.js
@@ -1,15 +1,15 @@
 const map = (projectionFn, collection, previous) => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const runner = (projectionFn, collection, previous) => {
       if (collection.length !== 1) {
-        projectionFn(collection.slice(0, 1))
+        projectionFn(collection.slice(0, 1).join())
           .then(val => {
             runner(projectionFn, collection.slice(1), previous.concat(val))
-          })
+          }).catch(e => reject(e))
       }
       else {
-        projectionFn(collection.slice(0, 1))
-          .then(val => resolve(previous.concat(val)))
+        projectionFn(collection.slice(0, 1).join())
+          .then(val => resolve(previous.concat(val))).catch(e => reject(e))
       }
     }
     runner(projectionFn, collection, previous)
