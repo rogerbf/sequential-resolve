8f66e87a8adf321d44fc3aaf2ae5be8157e3fa1c
rogerbf <roger@bergfalk.nu>
2017-04-29 17:21:04 +0200

    custom promise implementation never actually referenced

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const resolve = (tasks = [], P = Promise) => {
-  return new Promise((resolve, reject) => {
+  return new P((resolve, reject) => {
     const run = (queue = tasks, results = []) => {
-      Promise.resolve(
-        queue.length > 0 ? queue.slice(0, 1).pop() : Promise.reject()
+      P.resolve(
+        queue.length > 0 ? queue.slice(0, 1).pop() : P.reject()
       )
       .then(
         next =>
-          Promise.resolve(typeof (next) === `function` ? next() : next)
+          P.resolve(typeof (next) === `function` ? next() : next)
           .then(data => run(queue.slice(1), [ ...results, data ]))
         ,
         resolve.bind(null, results)
